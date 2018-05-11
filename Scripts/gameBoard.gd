9c8bb988efde234ae134b694945bed80d0db0466
chrisvillan <villanuevachris19@gmail.com>
2018-05-10 18:13:38 -0700

    added algorithm analysis pdf

--- Scripts/gameBoard.gd
+++ Scripts/gameBoard.gd
@@ -183,7 +183,7 @@ func _process(delta):
 			setup_game()
 	
 	if Input.is_action_pressed("ui_cancel"):
-		get_tree().change_scene("res://Game.xml")
+		get_tree().reload_current_scene()
 	
 	timer -= delta
 	
